c6759e6cc69d8df98cc574b9bc261a98f4970adf
Changrui Zhou <changruizzzz@gmail.com>
2019-09-23 14:34:17 -0400

    new ui for dashboard

---
+++ src/main/resources/static/resources/js/dashboard.js
@@ -0,0 +1,10 @@
+// $(document).ready(function () {
+//     $("#logout-link").on("click",function(e) {
+//         e.preventDefault(); // cancel the link itself
+//         alert(this.href);
+//
+//         $.post(this.href,function(data) {
+//
+//         });
+//     });
+// });
